78b1ecee5fc977ed4481950421ff0a6febe9f75a
Isaac Zylstra <izylstra@gmail.com>
2015-05-27 10:46:34 -0400

    add semicolon from last commit

--- Kinect2Scratch.js
+++ Kinect2Scratch.js
@@ -10,7 +10,7 @@
 
 	ext.my_first_block = function() {
 		alert("Hello!");
-        console.log("hello, world.")
+        console.log("hello, world.");
     };
 	
 	    ext.power = function(base, exponent) {
