5e6a159a33d9219d6d2d5a2f2bea48146247be34
Isaac Zylstra <izylstra@gmail.com>
2015-05-27 11:46:59 -0400

    Try a drop down box

--- Kinect2Scratch.js
+++ Kinect2Scratch.js
@@ -1,28 +1,38 @@
 (function(ext) {
     // Cleanup function when the extension is unloaded
     ext._shutdown = function() {};
+    
 
     // Status reporting code
     // Use this to report missing hardware, plugin or unsupported browser
     ext._getStatus = function() {
         return {status: 2, msg: 'Ready'};
     };
+    
+        // Block and block menu descriptions
+    var descriptor = {
+        blocks: [
+			['', 'My First Block', 'my_first_block'],
+			['r', '%n ^ %n', 'power', 2, 3],
+            ['r', '%m.k', 'k', 'headx']
+        ]
+    };
 
 	ext.my_first_block = function() {
         console.log("hello, world.");
     };
 	
-	    ext.power = function(base, exponent) {
+    ext.power = function(base, exponent) {
         return Math.pow(base, exponent);
     };
 
-	
-    // Block and block menu descriptions
-    var descriptor = {
-        blocks: [
-			['', 'My First Block', 'my_first_block'],
-			['r', '%n ^ %n', 'power', 2, 3],
-        ]
+    ext.k = function(m) {
+        switch(m){
+            case 'headx':
+            case 'heady':
+            case 'righthandx':
+            case 'righthandy':
+        }
     };
 
     // Register the extension
