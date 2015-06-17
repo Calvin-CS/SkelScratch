8ec4fdd1f6ce68351701a41d6a98cf5929a96cb5
Isaac Zylstra <izylstra@gmail.com>
2015-06-17 09:43:15 -0400

    Add in client code

--- Kinect2Scratch.js
+++ Kinect2Scratch.js
@@ -6,6 +6,38 @@
     alert("BEFORE CLICKING OK<br/>Make sure you have have followed the instructions in Kinect2Scratch");
     console.log("Right after the alert");
     
+    var inc = document.getElementById('incomming');
+            var wsImpl = window.WebSocket || window.MozWebSocket;
+            //var form = document.getElementById('sendForm');
+            //var input = document.getElementById('sendText');
+            
+            inc.innerHTML += "connecting to server ..<br/>";
+
+            // create a new websocket and connect
+            window.ws = new wsImpl('ws://localhost:8181/');
+
+            // when data is comming from the server, this metod is called
+            ws.onmessage = function (evt) {
+                inc.innerHTML += evt.data + '<br/>';
+            };
+
+            // when the connection is established, this method is called
+            ws.onopen = function () {
+                inc.innerHTML += '.. connection open<br/>';
+            };
+
+            // when the connection is closed, this method is called
+            ws.onclose = function () {
+                inc.innerHTML += '.. connection closed<br/>';
+            }
+            
+			//form.addEventListener('submit', function(e){
+			//	e.preventDefault();
+			//	var val = input.value;
+			//	ws.send(val);
+			//	input.value = "";
+			//});
+    
     // Cleanup function when the extension is unloaded
     ext._shutdown = function() {};
     
