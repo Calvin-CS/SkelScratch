2b44a47c1fac5a48865933ff923f7a9efa9ff86f
Isaac Zylstra <izylstra@gmail.com>
2015-07-23 15:25:57 -0400

    Debugging stuff

--- KinectinScratch.js
+++ KinectinScratch.js
@@ -329,9 +329,12 @@
         }
         
         switch(x){
-            case 'x': return jsonObject.bodies[b].joints[a].x*xScale;
-            case 'y': return jsonObject.bodies[b].joints[a].y*yScale;
-            case 'z': return jsonObject.bodies[b].joints[a].z*zScale;
+            case 'x': return b;
+            case 'y': return a;
+            case 'z': return 3;
+            //case 'x': return jsonObject.bodies[b].joints[a].x*xScale;
+            //case 'y': return jsonObject.bodies[b].joints[a].y*yScale;
+            //case 'z': return jsonObject.bodies[b].joints[a].z*zScale;
         }
     }
 
